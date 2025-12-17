c5e766bc4c0fb380f058df8cdad19a192f6c35b8
Qingnan Zhou <qnzhou@gmail.com>
2025-12-17 13:52:32 +0800

    Update mtetcol.

--- cmake/mtetcol.cmake
+++ cmake/mtetcol.cmake
@@ -6,6 +6,6 @@ include(CPM)
 CPMAddPackage(
   NAME mtetcol
   GITHUB_REPOSITORY adobe-research/mtetcol
-  GIT_TAG 8901d44292bd3bbc9c5fe36654c780f5040a06c9
+  GIT_TAG c0e7253dd59f5f20b37f0bff541b86d226b83ac5
 )
 

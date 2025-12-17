8fd616743e988cf9652969fcce66cbbe935863de
Qingnan Zhou <qnzhou@gmail.com>
2025-12-17 22:37:57 +0800

    Update stf.

--- cmake/stf.cmake
+++ cmake/stf.cmake
@@ -6,6 +6,5 @@ include(CPM)
 CPMAddPackage(
     NAME stf
     GITHUB_REPOSITORY adobe-research/space-time-functions
-    GIT_TAG 1e16987a3dfa23b9c90728f6f942cfb25a6505a5
+    GIT_TAG 80da61c0a693b7ef96a0150a8165cf2e6d546643
 )
-
